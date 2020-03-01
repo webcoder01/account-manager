b3d1fa0ebd629c6bcb4d1e960846ffc29c9fa9a3
Antoine Raymond <antoine.raym@orange.fr>
2020-03-02 00:40:30 +0100

    Constants

---
+++ src/Utils/Constants.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Utils;
+
+
+class Constants
+{
+    const FLASH_SUCCESS = 'success';
+    const FLASH_WARNING = 'warning';
+    const FLASH_NOTICE = 'notice';
+
+    const RESET_TOKEN_LIFETIME = 86400; // 24 hours
+}
